8f3216769a4ebb21a120f2612cb5845266037135
Nancy dashore <84832797+nancy0709-stack@users.noreply.github.com>
2024-12-02 19:27:48 -1200

    Add files via upload

---
+++ hms.sql
@@ -0,0 +1,29 @@
+CREATE TABLE user_login (
+    id VARCHAR(100)  PRIMARY KEY NOT NULL ,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+
+
+INSERT INTO user_login (id, username, password)
+VALUES ('1', 'zahid', 'zahid');
+
+CREATE TABLE doctor_record (
+    id VARCHAR(100) primary key NOT NULL,
+    DoctorName TEXT(100) NOT NULL,
+    Specialization TEXT(100) NOT NULL
+);
+SELECT * FROM doctor_record WHERE id = '01';
+
+
+select * from doctor_record;
+
+CREATE TABLE patient_record (
+    id VARCHAR(50) primary key NOT NULL,
+    Name VARCHAR(100) NOT NULL,
+    Disease VARCHAR(100) NOT NULL,
+    Date VARCHAR(30) NOT NULL
+);
+select * from patient_record;
+
